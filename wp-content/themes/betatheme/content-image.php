073cae856a69c4982e777425c40068d6f493f227
sarich16 <pgjr200000@gmail.com>
2016-03-03 20:24:34 +0100

    pt.7 add + create post formats

---
+++ wp-content/themes/betatheme/content-image.php
@@ -0,0 +1,3 @@
+<h3>Image POST: <?php the_title(); ?></h3>
+<div class="thumbnail-img"><?php the_post_thumbnail('thumbnail'); ?></div>
+<hr>
--- wp-content/themes/betatheme/index.php
+++ wp-content/themes/betatheme/index.php
@@ -4,15 +4,13 @@
 
 if( have_posts() ):
 	
-	while( have_posts() ): the_post(); ?>
+	while( have_posts() ): the_post(); 
+	//while( have_posts() ): the_post(); echo 'THIS IS THE FORMAT: '.get_post_format(); // GET Format Example 
+	?>
 		
-		<h3><?php the_title(); ?></h3>
-		<div class="thumbnail-img"><?php the_post_thumbnail('thumbnail'); ?></div>
-		<small>Posted on: <?php the_time('F j, Y') ?> at <?php the_time('g:i a') ?>, in <?php the_category(); ?></small>
+		<?php get_template_part('content', get_post_format()); //Looks for content-aside, content-picture, or content-video  ?>
 
-		<p><?php the_content(); ?></p>
 
-		<hr>
 <?php	endwhile;
 
 endif;
